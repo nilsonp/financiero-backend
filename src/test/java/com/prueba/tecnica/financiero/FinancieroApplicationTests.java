f950c86092df5cb486707a3ce4eecfd2481d1b1d
Nilson Parra <nilsonp@gmail.com>
2025-12-20 09:05:39 -0500

    Commit inicial

---
+++ src/test/java/com/prueba/tecnica/financiero/FinancieroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.tecnica.financiero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancieroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
