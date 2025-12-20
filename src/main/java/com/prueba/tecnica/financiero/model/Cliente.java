dbec8e9aa80ba528408286da70d8b568d397c291
Nilson Parra <nilsonp@gmail.com>
2025-12-20 16:21:30 -0500

    Entidades y repositorios JPA

--- src/main/java/com/prueba/tecnica/financiero/model/Cliente.java
+++ src/main/java/com/prueba/tecnica/financiero/model/Cliente.java
@@ -1,7 +1,21 @@
 package com.prueba.tecnica.financiero.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
